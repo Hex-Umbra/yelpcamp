309bd4c0985c5792c5f3b300a8ae9f011ad9df42
Hex-Umbra <g.p.dacosta@hotmail.com>
2024-09-19 11:37:36 +0200

    Added a footer and a navbar. Added an image, a description and a random price for each of the randomly generated campgrounds

--- seeds/index.js
+++ seeds/index.js
@@ -19,9 +19,14 @@ const seedDb = async () => {
   await Campground.deleteMany({});
   for (let i = 0; i < 50; i++) {
     const random1000 = Math.floor(Math.random() * 1000);
+    const price = Math.floor(Math.random() * 20) + 10;
     const camp = new Campground({
       location: `${cities[random1000].city}, ${cities[random1000].state}`,
       title: `${sample(descriptors)} ${sample(places)}`,
+      image: `https://picsum.photos/400?random${Math.random()}`,
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat quisquam quasi reprehenderit corporis cumque voluptatum molestiae necessitatibus sint aliquam in cum consequatur, praesentium blanditiis iusto debitis accusantium natus quia dignissimos?",
+      price,
     });
     await camp.save();
   }
