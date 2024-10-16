6456f8b58fb4e1f3ac1826347c969f8926c3a827
Hex-Umbra <g.p.dacosta@hotmail.com>
2024-10-16 10:02:50 +0200

    Added Error handling middleware and utilities, Added server-side validation with a new package 'JOI' on our post route

--- routes/campgoundRoutes.js
+++ routes/campgoundRoutes.js
@@ -1,67 +1,90 @@
 const express = require("express");
 const campgroundModel = require("../models/campgroundModel.js");
+const catchAsync = require("../Utils/catchAsync");
+const ExpressError = require("../Utils/ExpressError.js");
 const router = express.Router();
+const Joi = require("joi");
 
-router.get("/campgrounds", async (req, res) => {
-  try {
+router.get(
+  "/campgrounds",
+  catchAsync(async (req, res, next) => {
     const campgrounds = await campgroundModel.find();
     res.render("campgrounds/index", { campgrounds });
-  } catch (err) {
-    console.log(err.message);
-    res.status(500).json({ message: err.message });
-  }
-});
+  })
+);
 router.get("/campgrounds/new", async (req, res) => {
-  try {
-    res.render("campgrounds/new");
-  } catch (err) {
-    console.log(err.message);
-    res.status(500).json({ message: err.message });
-  }
+  res.render("campgrounds/new");
 });
-router.post("/campgrounds", async (req, res) => {
-  try {
+router.post(
+  "/campgrounds",
+  catchAsync(async (req, res, next) => {
+    // if (!req.body.campground) throw new ExpressError("Invalid Data", 400);
+    const campgroundSchema = Joi.object({
+      campground: Joi.object({
+        title: Joi.string().required(),
+        price: Joi.number().required().min(0),
+        location: Joi.string().required(),
+        image: Joi.string().required(),
+        description: Joi.string().required(),
+      }).required(),
+    });
+    const { error } = campgroundSchema.validate(req.body);
+    if (error) {
+      const msg = error.details.map((el) => el.message).join(",");
+      throw new ExpressError(msg, 400);
+    }
     const newCampground = new campgroundModel(req.body.campground);
     await newCampground.save();
-    res.redirect(`/campgrounds/${newCampground._id}`)
-  } catch (error) {
-    res.status(500).json({ message: err.message });
-  }
-});
-router.get("/campgrounds/:id", async (req, res) => {
-  try {
+    res.redirect(`/campgrounds/${newCampground._id}`);
+  })
+);
+router.get(
+  "/campgrounds/:id",
+  catchAsync(async (req, res) => {
     const campground = await campgroundModel.findById(req.params.id);
     res.render("campgrounds/show", { campground });
-  } catch (err) {
     console.log(err.message);
     res.status(500).json({ message: err.message });
-  }
-});
-router.get("/campgrounds/:id/edit", async(req,res)=>{
-  try {
+  })
+);
+router.get(
+  "/campgrounds/:id/edit",
+  catchAsync(async (req, res) => {
     const campground = await campgroundModel.findById(req.params.id);
     res.render("campgrounds/edit", { campground });
+    res.status(500).json({ message: err.message });
+  })
+);
+router.put(
+  "/campgrounds/:id",
+  catchAsync(async (req, res) => {
+    const { id } = req.params;
+    const campground = await campgroundModel.findByIdAndUpdate(id, {
+      ...req.body.campground,
+    });
+    res.redirect(`/campgrounds/${campground._id}`);
+    res.status(500).json({ message: err.message });
+  })
+);
+router.delete("/campgrounds/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deletedCampground = await campgroundModel.findByIdAndDelete(id);
+    res.redirect("/campgrounds");
   } catch (error) {
     res.status(500).json({ message: err.message });
   }
-})
-router.put("/campgrounds/:id/", async(req,res)=>{
-  try {
-    const {id} = req.params;
-    const campground = await campgroundModel.findByIdAndUpdate(id,{...req.body.campground})
-    res.redirect(`/campgrounds/${campground._id}`)
-  } catch (err) {
-    res.status(500).json({ message: err.message });
-  }
-})
-router.delete("/campgrounds/:id", async(req,res)=>{
-try {
-    const {id} = req.params;
-    const deletedCampground = await campgroundModel.findByIdAndDelete(id);
-    res.redirect("/campgrounds")
-} catch (error) {
-  res.status(500).json({ message: err.message });
-}  
-})
+});
+
+//
+router.all("*", (req, res, next) => {
+  next(new ExpressError("Page Not Found", 404));
+});
+router.use((err, req, res, next) => {
+  const { status = 500 } = err;
+  if (!err.message) err.message = "Oh no, Something Went Wrong";
+  res.status(status).render("../views/error", { err });
+});
+//
 
 module.exports = router;
